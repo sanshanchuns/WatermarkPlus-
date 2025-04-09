bc9a2332e15aae1bd9f469d28c00a298e7c25a92
leo <zhuli@kuaishou.com>
2025-04-09 19:28:05 +0800

    Initial Commit

---
+++ WatermarkPlus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatermarkPlus
+//
+//  Created by leozhu on 2025/4/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatermarkPlus/WatermarkPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatermarkPlusApp.swift
+//  WatermarkPlus
+//
+//  Created by leozhu on 2025/4/9.
+//
+
+import SwiftUI
+
+@main
+struct WatermarkPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
